12db797f361cd1e70cf5aa7596aa659310046f4c
Ilaria Roglieri (XPS) <ilariaroglieri@gmail.com>
2021-10-12 11:39:07 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+//kill gutenberg
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+//remove admin bar
+show_admin_bar(false);
+
+function register_my_menu() {
+	register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+
+add_action( 'init', 'register_my_menu' );
+
+
+function jquery_scripts() {
+	wp_enqueue_script( 'custom', get_stylesheet_directory_uri() . '/custom.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'jquery_scripts' );
+
+add_theme_support( 'post-thumbnails' ); 
+
+remove_filter('the_content', 'wpautop');
+
+// img attachment defaults
+
+function default_attachment_display_settings() {
+    update_option( 'image_default_link_type', 'none' );
+    update_option( 'image_default_size', 'full' );
+}
+
+add_action( 'after_setup_theme', 'default_attachment_display_settings' );
+
+
+?>
