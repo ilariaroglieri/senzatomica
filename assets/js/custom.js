a6570459322a2f1f0e7490d8b206c669cfe9965f
Ilaria Roglieri (XPS) <ilariaroglieri@gmail.com>
2021-10-18 13:24:35 +0200

    building home page: random type

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,18 +40,16 @@ jQuery(document).ready(function($) {
   // shuffle images
   var arr = $('.grid-item');
   shuffle(arr);
-  console.log(arr);
   $('#image-grid').empty().append(arr);
-
-
-  
 
   // hide on hover
   $('.grid-item').hover(function(i, el) {
     $(this).addClass('hidden');
-
-
   });
+
+
+
+ 
 
 // --- header behaviour
 	
@@ -70,12 +68,10 @@ jQuery(document).ready(function($) {
 	 //  prevScrollPos = currentScrollPos;
   // });
 
-
-
 // --- Hamburger menu
   $('.menu-toggle').click(function() {
     $(this).toggleClass('open');
-    $('div[class*="menu-1"]').toggleClass('active');
+    $('div[class*="menu-main"]').toggleClass('active');
     // check if it's on slider
   	if ( $('body').hasClass('home') && $(window).scrollTop() < window.innerHeight ) {
   		$('#logo').addClass('visible')
@@ -87,6 +83,55 @@ jQuery(document).ready(function($) {
     	$('#logo').removeClass('visible');
     }
   });
+
+
+  // BREAK SENTENCE IN WORDS
+  var variableType = $('.variable-type').find('h2').text();
+
+  var splitWords = variableType.split(" ");
+  $('.variable-type').find('h2').empty();
+
+  for (var i = 0; i < splitWords.length; i++) {
+    $('.variable-type').find('h2').append('<div>'+splitWords[i]+'</div>');
+    // var splitLetters = splitWords[i].split('');
+  }
+
+  // BREAK WORDS IN LETTERS
+  $('.variable-type div h2 > div').each(function(i, el) {
+    var splitLetters = $(el).text().split("");
+    console.log(splitLetters);
+
+    if (splitLetters.length <= 3) {
+      $(el).addClass('condensed');
+    } else if (splitLetters.length >= 4 && splitLetters.length < 6) {
+      $(el).addClass('extended');
+    } else if (splitLetters.length >= 6 && splitLetters.length <= 8) {
+      $(el).addClass('regular');
+    } else if (splitLetters.length > 8) {
+      $(el).empty();
+
+      for (var i = 0; i < splitLetters.length; i++) {
+        $(el).append('<span class="extended">'+splitLetters[i]+'</span>');
+
+        var weights = ['xcondensed','condensed','regular','extended'];
+
+        var letters = $(el).find('span');
+        var randomLetters = letters[Math.floor(Math.random() * (letters.length - 1))];
+
+        $(letters[0]).removeClass('extended').addClass('xcondensed');
+        $(letters[1]).removeClass('extended').addClass('xcondensed');
+        $(letters[2]).removeClass('extended').addClass('condensed');
+        $(letters[3]).removeClass('extended').addClass('condensed');
+        $(letters[4]).removeClass('extended').addClass('regular');
+        $(letters[5]).removeClass('extended').addClass('regular');
+
+        // $(randomLetters).addClass(weights[Math.floor(Math.random() * weights.length)]);
+      }
+    }
+  });
+
+  // ASSIGN RANDOM CLASSES TO SOME LETTERS
+
 
 
 
--- front-page.php
+++ front-page.php
@@ -3,49 +3,70 @@
 <section class="content" id="content-home">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <?php $images = get_field('immagini_griglia');
-      if( $images ): ?>
-        <div id="image-grid-container" class="container-fluid p-absolute full-width full-height">
-          <div id="image-grid" class="d-grid">
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-            <?php foreach( $images as $image ): ?>
-              <div class="grid-item">
-                <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
-              </div>
-            <?php endforeach; ?>
-          </div>
+    <?php $images = get_field('immagini_griglia');
+    if( $images ): ?>
+      <div id="image-grid-container" class="container-fluid p-absolute full-width full-height">
+        <div id="image-grid" class="d-grid">
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="grid-item">
+              <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['title']); ?>" />
+            </div>
+          <?php endforeach; ?>
         </div>
-      <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class('container t-center d-flex d-column'); ?>>
+      <?php
+        $subtitle = get_field('sottotitolo');
+        $sommario = get_field('sommario');
+      ?>
+
+      <div class="variable-type spacing-t-2 d-flex flex-row">
+        <div class="d-one-twelfth t-whole"></div>
+        <div class="d-ten-twelfth t-whole">
+          <h2 class="normal s-large uppercase"><?= $subtitle; ?></h2>
+        </div>
+        <div class="d-one-twelfth t-whole"></div>
+      </div>
+
+      <div class="summary spacing-t-2 d-flex flex-row">
+        <div class="d-two-twelfth t-whole"></div>
+        <div class="d-eight-twelfth t-whole">
+          <p class="sans s-regular"><?= $sommario; ?></p>
+        </div>
+        <div class="d-two-twelfth t-whole"></div>
+      </div>
     </article>
   
   <?php endwhile; else: ?>
