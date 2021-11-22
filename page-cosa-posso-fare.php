3d9c8deb80f55abdc3252cd9ea077ff9612a4d77
Ilaria Roglieri (XPS) <ilariaroglieri@gmail.com>
2021-11-22 13:33:59 +0100

    cosa posso fare

--- page-cosa-posso-fare.php
+++ page-cosa-posso-fare.php
@@ -5,13 +5,13 @@
 
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       <div class="container">
-        <h2 class="uppercase condensed italic s-huge t-center spacing-b-1"><?php the_title(); ?></h2>
+        <h2 class="uppercase condensed italic s-gigantic t-center spacing-b-1"><?php the_title(); ?></h2>
 
         <?php if( have_rows('sezione_cosa_posso_fare') ): ?>
-          <div class="d-flex v-center">
+          <div class="cpf-selector d-flex v-center">
             <?php $i = 0; ?>
             <?php while( have_rows('sezione_cosa_posso_fare') ): the_row(); ?>
-              <!-- 3 sezioni cosa posso fare -->
+              <!-- titoli 3 sezioni cosa posso fare -->
               <?php 
               $title = get_sub_field('titolo_sezione_cpf'); 
               ?>
@@ -24,6 +24,46 @@
         <?php endif; ?>
       </div>
 
+
+     <?php if( have_rows('sezione_cosa_posso_fare') ): ?>
+        <?php $i = 0; ?>
+        <?php while( have_rows('sezione_cosa_posso_fare') ): the_row(); ?>
+          <!-- 3 sezioni cosa posso fare -->
+          <?php 
+          $title = get_sub_field('titolo_sezione_cpf'); 
+          ?>
+
+          <div data-section="<?= $title; ?>" class="container-section <?php if ($i == 0): echo 'active'; endif; ?>">
+
+            <?php if( have_rows('tipo_sezione') ): ?>
+              <?php while( have_rows('tipo_sezione') ): the_row(); ?>
+                <?php 
+                  $title = get_sub_field('titolo_sezione'); 
+                  $titleSize = get_sub_field('dimensione_titolo'); 
+                  $listType = get_sub_field('stile_lista'); 
+                  $color = get_sub_field('colore'); 
+                ?>
+
+                <div class="container-fluid" style="background-color: <?php echo $color; ?>">
+                  <div class="container full-height spacing-p-t-3 spacing-p-b-3">
+                    <!-- titolo + lista link -->
+                    <?php if( get_row_layout() == 'titolo_lista_link' ): ?>
+                      <h4 class="uppercase xcondensed <?php echo $titleSize; ?>"><?= $title; ?></h4>
+
+                    <?php elseif( get_row_layout() == 'contattaci' ): ?>
+                      <h4 class="uppercase xcondensed <?php echo $titleSize; ?>"><?= $title; ?></h4>
+                    <?php endif; ?>
+                  </div>
+                </div>
+
+              <?php endwhile; ?>
+            <?php endif; ?>
+
+          </div>
+
+          <?php $i++; ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
     </article>
 
   <?php endwhile; else: ?>
